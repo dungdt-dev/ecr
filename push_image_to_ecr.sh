3a495fef4c733c6c27fb68db85d416466c759773
dungdt <dungdt.ytb@gmail.com>
2024-08-19 22:54:33 +0700

    test run sh file

--- push_image_to_ecr.sh
+++ push_image_to_ecr.sh
@@ -1,5 +1,5 @@
 #Log in to Amazon ECR
-def ecrLogin = sh(script: 'aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 022499014177.dkr.ecr.ap-southeast-1.amazonaws.com', returnStdout: true).trim()
+aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 022499014177.dkr.ecr.ap-southeast-1.amazonaws.com
 echo "Logged in to ECR"
 
 #Push Docker image to Amazon ECR
