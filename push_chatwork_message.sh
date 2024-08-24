81a41c5f4003c41bc3f1babdf1e5a84deddab10f
dungdt <dungdt.ytb@gmail.com>
2024-08-24 16:37:07 +0700

    ecr

--- push_chatwork_message.sh
+++ push_chatwork_message.sh
@@ -1,5 +1,6 @@
 CHATWORK_API_TOKEN="$1"
 CHATWORK_ROOM_ID="$2"
 BODY="$3"
+ENCODED_BODY=$(echo "$BODY" | jq -sRr @uri)
 
-curl -k -X POST -H "X-ChatWorkToken: ${CHATWORK_API_TOKEN}" -d "body=${BODY}" "https://api.chatwork.com/v2/rooms/${CHATWORK_ROOM_ID}/messages"
+curl -k -X POST -H "X-ChatWorkToken: ${CHATWORK_API_TOKEN}" -d "body=${ENCODED_BODY}" "https://api.chatwork.com/v2/rooms/${CHATWORK_ROOM_ID}/messages"
