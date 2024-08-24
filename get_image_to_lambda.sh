db4293a0267caa1f2d5f3f36fb0e5d675ffc9781
dungdt <dungdt.ytb@gmail.com>
2024-08-25 02:51:33 +0700

    ecr fix bug

--- get_image_to_lambda.sh
+++ get_image_to_lambda.sh
@@ -5,17 +5,11 @@ NEW_VERSION_TAG="$3"
 ECR_URI=$(echo "$ECR" | jq -r '.ecr_uri')
 IMAGE_NAME=$(echo "$ECR" | jq -r '.name')
 
-# Chia danh sách các hàm Lambda bằng dấu phẩy
-IFS=',' read -r -a array <<< "$LIST_LAMBDAS"
+echo "$LIST_LAMBDAS" | jq -c '.[]' | while read -r lambda; do
+    name=$(echo "$lambda" | jq -r '.name')
+    region=$(echo "$lambda" | jq -r '.region')
 
-# Lặp qua từng phần tử trong danh sách
-for element in "${array[@]}"
-do
-    IFS=':' read -r -a lambda <<< "$element"
-    name=${lambda[0]}
-    region=${lambda[1]}
-
-   aws lambda update-function-code \
-   --function-name $name \
-   --image-uri ${ECR_URI}/${IMAGE_NAME}:${NEW_VERSION_TAG} --region $region
+    aws lambda update-function-code \
+       --function-name $name \
+       --image-uri ${ECR_URI}/${IMAGE_NAME}:${NEW_VERSION_TAG} --region $region
 done
--- index.js
+++ index.js
@@ -31,7 +31,7 @@ exports.handler = async (event) => {
         statusCode: 200,
         body: {
             rows: rows,
-            content: 'test pull image dynamic config'
+            content: 'test pull image dynamic v4'
         },
     };
 };
