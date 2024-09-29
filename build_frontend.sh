39febffda2d7b387b2a319fbca742794a11ea44e
dungdt <dungdt.ytb@gmail.com>
2024-09-29 16:42:48 +0700

    test

--- build_frontend.sh
+++ build_frontend.sh
@@ -21,34 +21,34 @@ docker build -t ${IMAGE_NAME}:${NEW_VERSION_TAG} .
 docker run -it -d --name ${IMAGE_NAME} ${IMAGE_NAME}:${NEW_VERSION_TAG} sh
 
 exec_result=$(docker exec ${IMAGE_NAME} sh -c "
-						git config --global init.defaultBranch master &&
-						mkdir deploy &&
+                        git config --global init.defaultBranch master &&
+                        mkdir deploy &&
                         cd deploy &&
                         git init &&
-						git config --global user.name '${USER_NAME}' &&
-						git config --global user.email '${USER_EMAIL}' &&
+                        git config --global user.name '${USER_NAME}' &&
+                        git config --global user.email '${USER_EMAIL}' &&
                         git remote add origin ${REMOTE_ORIGIN} &&
                         git remote -v &&
-						git fetch &&
-						git checkout ${BRANCH} &&
-						cd /var/task &&
-						cp -r build1/* deploy/ &&
-						cd deploy &&
-						if [ -n \"\$(git status --porcelain)\" ]; then
-              git add . &&
-              git commit -m '${NEW_VERSION_TAG}' &&
-              git push
-            else
-                echo 'No changes to commit'
-            fi
-            ")
+                        git fetch &&
+                        git checkout ${BRANCH} &&
+                        cd /var/task &&
+                        cp -r build1/* deploy/ &&
+                        cd deploy &&
+                        if [ -n \"\$(git status --porcelain)\" ]; then
+                          git add . &&
+                          git commit -m '${NEW_VERSION_TAG}' &&
+                          git push
+                        else
+                          echo 'No changes to commit'
+                        fi
+            ") || exec_result=1
 
 docker rm -f ${IMAGE_NAME}
 docker rmi ${IMAGE_NAME}:${NEW_VERSION_TAG}
 
 
 # error
-if [ $? -ne 0 ]; then
-    echo "Error occurred during docker exec: $exec_result"
-        exit 1
+if [ $exec_result -ne 0 ]; then
+    echo "An error occurred during the exec command."
+    exit 1
 fi
