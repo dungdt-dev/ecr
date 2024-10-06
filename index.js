3230c98040352236d3379287cbec6c1fb755b70e
dungdt <dungdt.ytb@gmail.com>
2024-10-06 21:45:45 +0700

    update

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ exports.handler = async (event) => {
         statusCode: 200,
         body: {
             rows: rows,
-            content: 'test pull image dynamic test region 6/10 lambda_test'
+            content: 'test pull image dynamic test region 6/10 lambda_test hoang'
         },
     };
 };
