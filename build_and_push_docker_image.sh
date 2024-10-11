6adcc6f323eeadfc2d528f4a9a54956968f5ab62
dungdt <dungdt.ytb@gmail.com>
2024-10-12 00:53:03 +0700

    update

--- build_and_push_docker_image.sh
+++ build_and_push_docker_image.sh
@@ -1,28 +1,4 @@
 #!/bin/bash
-#ECR="$1"
-#NEW_VERSION_TAG="$2"
-#ECR_URI=$(echo "$ECR" | jq -r '.ecr_uri')
-#IMAGE_NAME=$(echo "$ECR" | jq -r '.name')
-#REGION=$(echo "$ECR" | jq -r '.region')
-#
-##Remove front end
-#rm -rf build/
-#
-##Build
-#docker build -t ${IMAGE_NAME}:${NEW_VERSION_TAG} .
-#
-##Set tag
-#docker tag ${IMAGE_NAME}:${NEW_VERSION_TAG} ${ECR_URI}/${IMAGE_NAME}:${NEW_VERSION_TAG}
-#
-##Log in to Amazon ECR
-#aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ECR_URI}
-#
-##Push Docker image to Amazon ECR
-#docker push ${ECR_URI}/${IMAGE_NAME}:${NEW_VERSION_TAG}
-#
-##Remove image
-#docker rmi ${IMAGE_NAME}:${NEW_VERSION_TAG}
-#docker rmi ${ECR_URI}/${IMAGE_NAME}:${NEW_VERSION_TAG}
 
 LIST_ECR="$1"
 NEW_VERSION_TAG="$2"
@@ -39,8 +15,15 @@ for ecr in "${list_ecr[@]}"; do
     repository=$(echo "$ecr" | jq -r '.repository')
     region=$(echo "$ecr" | jq -r '.region')
 
-    #Build
-    docker build -t ${repository}:${NEW_VERSION_TAG} .
+    # Kiểm tra nếu image đã tồn tại
+    if [[ "$(docker images -q ${repository}:${NEW_VERSION_TAG} 2> /dev/null)" == "" ]]; then
+        echo "Image ${repository}:${NEW_VERSION_TAG} không tồn tại, tiến hành build."
+
+        # Build Docker image nếu nó không tồn tại
+        docker build -t ${repository}:${NEW_VERSION_TAG} .
+    else
+        echo "Image ${repository}:${NEW_VERSION_TAG} đã tồn tại, bỏ qua bước build."
+    fi
 
     #Set tag
     docker tag ${repository}:${NEW_VERSION_TAG} ${ecr_uri}/${repository}:${NEW_VERSION_TAG}
@@ -52,6 +35,5 @@ for ecr in "${list_ecr[@]}"; do
     docker push ${ecr_uri}/${repository}:${NEW_VERSION_TAG}
 
     #Remove image
-    docker rmi ${repository}:${NEW_VERSION_TAG}
     docker rmi ${ecr_uri}/${repository}:${NEW_VERSION_TAG}
 done
